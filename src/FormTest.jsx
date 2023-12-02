2b0866cf52c877649334f0f7155485d4fee6f4c9
Lᴜғғʏ <149653325+Kishor400@users.noreply.github.com>
2023-12-02 14:45:03 +0530

    change 1 on 2-12-2023

--- src/FormTest.jsx
+++ src/FormTest.jsx
@@ -2,27 +2,10 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import VideoImg from "./assets/VideoImg.jpg";
 
-import React, { useState, useEffect } from 'react';
-
+import formData from "./assets/FormTest.json";
 
 function FormTest() {
     
-  const [textData, setTextData] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(FormData);
-        const data = await response.text();
-        setTextData(data);
-        
-      } catch (error) {
-        console.error('Error fetching text file:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
     
 
@@ -34,9 +17,9 @@ function FormTest() {
           <div>
               <p>Latest</p>
               <div>
-                <Link to={textData.split("$")[1]}>{textData.split("$")[0]}</Link>
-                <Link to={textData.split("$")[3]}>{textData.split("$")[2]}</Link>
-                <Link to={textData.split("$")[5]}>{textData.split("$")[4]}</Link>
+                <Link to={FormTest.Test1.Link}>{FormTest.Test1.Title}</Link>
+                <Link to={FormTest.Test2.Link}>{FormTest.Test2.Title}</Link>
+                <Link to={FormTest.Test1.Link}>{FormTest.Test1.Title}</Link>
               </div>
               <button>More</button>
           </div>
