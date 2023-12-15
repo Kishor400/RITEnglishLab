69effba7835422d12e0b3bb8035b9672efe4598f
Lᴜғғʏ <149653325+Kishor400@users.noreply.github.com>
2023-12-15 11:23:30 +0000

    log in page updated

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,11 +1,39 @@
 // src/LoginPage.jsx
-import React, { useState } from 'react';
 import './LoginPage.css';
 import LogData from "./assets/Log.json";
 import Test from "./Test.jsx";
+import React, { useState, useEffect } from 'react';
 
 const LoginPage = () => {
 
+  useEffect(() => {
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+    const container = document.getElementById('container');
+
+    if (registerBtn && loginBtn && container) {
+      registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+      });
+
+      loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+      });
+    }
+    
+    return () => {
+      // Cleanup the event listeners when the component unmounts
+      if (registerBtn && loginBtn) {
+        registerBtn.removeEventListener('click', () => {
+          container.classList.add("active");
+        });
+
+        loginBtn.removeEventListener('click', () => {
+          container.classList.remove("active");
+        });
+      }
+    };
+  }, []);
   //document.querySelector(".TestPage").style="display:none;";
 
   const [Reg, setReg] = useState('');
@@ -29,7 +57,7 @@ const LoginPage = () => {
           console.log("Welcome");
           setInfo(LogData.Reg[i]+"$"+LogData.Name[i]);
           document.querySelector(".TestPage").style="display:flex;";
-          document.querySelector(".LogIn").style="display:none;";
+          document.querySelector(".Page").style="display:none;";
         }else{
           alert("Password Incorrect !");
         }
@@ -42,7 +70,7 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className='LogIn'>
+      {/*<div className='LogIn'>
         <form onSubmit={handleSubmit}>
           <input
             id="first_Reg"
@@ -63,11 +91,60 @@ const LoginPage = () => {
 
           <button type="submit">Submit form</button>
         </form>
+      </div>*/}
+      <div className='Page'>
+        <div className="container LogInPage" id="container">
+          <div className="form-container sign-up">
+              <form>
+                  <h1>Create Account</h1>
+                  <input type="text" placeholder="Name"/>
+                  <input type="email" placeholder="Email"/>
+                  <input type="password" placeholder="Password"/>
+                  <button>Sign Up</button>
+              </form>
+          </div>
+          <div className="form-container sign-in LogIn">
+              <form  onSubmit={handleSubmit}>
+                  <h1>Sign In</h1>
+                  <input 
+                    id="first_Reg" 
+                    name="first_Reg" 
+                    type="text" 
+                    onChange={event => setReg(event.target.value)} 
+                    value={Reg}
+                    placeholder='Reg'
+                  />
+                  <input
+                    id="last_Reg"
+                    name="last_Reg"
+                    type="password"
+                    value={Password}
+                    placeholder='Password'
+                    onChange={event => setPassword(event.target.value)}
+                  />
+                  <button type="submit">Sign In</button>
+              </form>
+          </div>
+          <div className="toggle-container">
+              <div className="toggle">
+                  <div className="toggle-panel toggle-left">
+                      <h1>Welcome Back!</h1>
+                      <p>Enter your personal details to use all of site features</p>
+                      <button className="hidden" id="login">Sign In</button>
+                  </div>
+                  <div className="toggle-panel toggle-right">
+                      <h1>Hello, Friend!</h1>
+                      <p>Register with your personal details to use all of site features</p>
+                      <button className="hidden" id="register">Sign Up</button>
+                  </div>
+              </div>
+          </div>
+        </div>
       </div>
       <div className='TestPage'>
         <Test info={infoData}/>
       </div>
-      
+     
     </>
   );
 };
