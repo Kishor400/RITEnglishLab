38672e6912b63c70a443806c2c608acb76075ead
kishor400 <kishor404@outlook.in>
2023-12-29 14:41:21 +0530

    pannel

--- src/Admin/pannel.jsx
+++ src/Admin/pannel.jsx
@@ -1,16 +1,26 @@
 
 import React, { useState } from 'react';
-
+import Marks from "../assets/markData.json";
+import log from "../assets/Log.json";
 
 
 
 
 function Pannel() {
-
+    console.log(Marks);
      
     return (
     <>
-        <p>hi</p>
+        <div>
+          <div>
+            <div>
+              <p>Best Performer Student</p>
+            </div>
+          </div>
+          <div>
+            <p>l</p>
+          </div>
+        </div>
     </>
   )
 }
