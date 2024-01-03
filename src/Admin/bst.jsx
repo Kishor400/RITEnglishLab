c23ee8fa3abcfa81c3c3563393fd6abbd65fe0bd
kishor400 <kishor404@outlook.in>
2024-01-03 16:35:25 +0530

    admin

--- src/Admin/bst.jsx
+++ src/Admin/bst.jsx
@@ -1,48 +1,61 @@
-
 import React, { useState } from 'react';
-import Marks from "../assets/markData.json";
-import log from "../assets/Log.json";
-import QData from "../assets/QData.json";
-import "./admin.css";
-
-
-
+import Marks from '../assets/markData.json';
+import log from '../assets/Log.json';
+import QData from '../assets/QData.json';
+import './admin.css';
 
 function BST() {
-    var x=Marks;
-    var bst=[];
-    var n=QData.title.length;
+  const x = Marks;
+  const bst = [];
+  const n = QData.title.length;
 
-    for(var i=0;i<n;i++){
-      var y=[]
-      for(var j=0;j<Marks.length;j++){
-        var k=x[j];
-        console.log(k)
-        if(k[1]==i){
-          y.push(k)
-        }
+  for (let i = 0; i < n; i++) {
+    const y = [];
+    for (let j = 0; j < Marks.length; j++) {
+      const k = x[j];
+      console.log(k);
+      if (k[1] === i) {
+        y.push(k);
       }
-      if(y!=[]){
-        var max=y[0];
-        for(var l=0;l<y.length;l++){
-          var m=y[l];
-          if(m[2]>max[2]){
-            max=m;
-          }
-        }
-      }else{
-        console.log("None")
-      }
-      bst.push(max)
     }
 
-    console.log(bst)
-    
-     
+    if (y.length !== 0) {
+      let max = y[0];
+      for (let l = 0; l < y.length; l++) {
+        const m = y[l];
+        if (m[2] > max[2]) {
+          max = m;
+        }
+      }
+      console.log(max[0]);
+      bst.push(max);
+    } else {
+      console.log('None');
+      // Move the console.log statement here if you want to log 'None' when the array is empty.
+    }
+  }
+
+  console.log(bst);
+
     return (
     <>
+        <p className='bsttitle'>Best Performing Student</p>
         <div className='bstbox'>
-           
+        <ul>
+          <li className='topbst'>
+            <p  className='marktop'>Test Title</p>
+            <p className='regtop'>Register Number</p>
+            <p>Mark</p>
+          </li>
+          {bst.map((season) => (
+            <li className='bsttab'>
+              <p>{QData.title[season[1]]}</p>
+              <p>{season[0]}</p>
+              {/* <p>{log.Name[log.Reg.indexOf(season[0])]}</p> */}
+              <p>{season[2]}</p>
+            </li>
+          ))}
+        </ul>
         </div>
     </>
   )
--- src/Admin/pannel.jsx
+++ src/Admin/pannel.jsx
@@ -4,40 +4,12 @@ import Marks from "../assets/markData.json";
 import log from "../assets/Log.json";
 import QData from "../assets/QData.json";
 import BST from "./bst.jsx";
+import ANA from "./ana.jsx";
 
 
 
 
 function Pannel() {
-    var x=Marks;
-    var bst=[];
-    var n=QData.title.length;
-
-    for(var i=0;i<n;i++){
-      var y=[]
-      for(var j=0;j<Marks.length;j++){
-        var k=x[j];
-        console.log(k)
-        if(k[1]==i){
-          y.push(k)
-        }
-      }
-      if(y!=[]){
-        var max=y[0];
-        for(var l=0;l<y.length;l++){
-          var m=y[l];
-          if(m[2]>max[2]){
-            max=m;
-          }
-        }
-      }else{
-        console.log("None")
-      }
-      bst.push(max)
-    }
-
-    console.log(bst)
-    
      
     return (
     <>
@@ -49,6 +21,9 @@ function Pannel() {
           </div>
           <div>
             <BST/>
+          </div>
+          <div>
+            <ANA/>
           </div>
         </div>
     </>
