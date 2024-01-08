dab6706a0929f9cbec0ffa0793c470e912e07a1b
Lᴜғғʏ <149653325+Kishor400@users.noreply.github.com>
2024-01-08 17:39:45 +0000

    webtest home

--- src/WebTest.jsx
+++ src/WebTest.jsx
@@ -49,9 +49,9 @@ function WebTest(logInfo) {
          </div>
         <div className='tt'>
             <p>Assessment On Web</p>
-            <div>
+            <div className='tttt'>
                 <p>Latest</p>
-                <div className='Tests'>
+                {/*<div className='Tests'>
                     <div>
                       <a onClick={()=>chgTest(Latest[0])}>{Latest[0]}</a>
                     </div>
@@ -61,6 +61,61 @@ function WebTest(logInfo) {
                     <div>
                       <a onClick={()=>chgTest(Latest[2])}>{Latest[2]}</a>
                     </div>
+                </div>*/}
+                <div className="ag-format-container">
+                  <div className="ag-courses_box">
+                    <div className="ag-courses_item">
+                      <a onClick={()=>chgTest(Latest[0])} className="ag-courses-item_link">
+                        <div className="ag-courses-item_bg"></div>
+
+                        <div className="ag-courses-item_title">
+                          {Latest[0]}
+                        </div>
+
+                        <div className="ag-courses-item_date-box">
+                          Start:
+                          <span className="ag-courses-item_date">
+                            04.11.2022
+                          </span>
+                        </div>
+                      </a>
+                    </div>
+
+                    <div className="ag-courses_item">
+                      <a onClick={()=>chgTest(Latest[1])} className="ag-courses-item_link">
+                        <div className="ag-courses-item_bg"></div>
+
+                        <div className="ag-courses-item_title">
+                          {Latest[1]}
+                        </div>
+
+                        <div className="ag-courses-item_date-box">
+                          Start:
+                          <span className="ag-courses-item_date">
+                            04.11.2022
+                          </span>
+                        </div>
+                      </a>
+                    </div>
+
+                    <div className="ag-courses_item">
+                      <a onClick={()=>chgTest(Latest[2])} className="ag-courses-item_link">
+                        <div className="ag-courses-item_bg"></div>
+
+                        <div className="ag-courses-item_title">
+                          {Latest[2]}
+                        </div>
+
+                        <div className="ag-courses-item_date-box">
+                          Start:
+                          <span className="ag-courses-item_date">
+                            31.10.2022
+                          </span>
+                        </div>
+                      </a>
+                    </div>
+
+                  </div>
                 </div>
 
                 <button>More</button>
